7a2473f5245bdf1eb1dd0d0b2a71c96fe2780692
astronavigator98 <astronavigator98@gmail.com>
2019-11-11 11:19:11 +0000

    второе задание не работает

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int j = strlen(str) - 1;
+    int n = j / 2;
+    for (int i = 0; i < n; i++, j--)
+    {
+        *(str + i) ^= *(str + j);
+        *(str + j) ^= *(str + i);
+        *(str + i) ^= *(str + j);
+    }
 }
 
