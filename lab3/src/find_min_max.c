47e79a17f3ba02b12ba35ce948956e4923c38283
astronavigator98 <astronavigator98@gmail.com>
2019-11-14 14:06:18 +0000

    Выполнена лабораторная работа 3

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -1,4 +1,5 @@
 #include "find_min_max.h"
+#include "utils.h"
 
 #include <limits.h>
 
@@ -6,7 +7,11 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
-
-  // your code here
+    for(int i=begin;i<end;i++){
+        if(min_max.min>array[i])
+            min_max.min=array[i];
+        if(min_max.max<array[i])
+            min_max.max=array[i];
+    }
   return min_max;
 }
--- lab3/src/sequential_min_max.c
+++ lab3/src/sequential_min_max.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "find_min_max.h"
-#include "utils.h"
 
-int main(int argc, char **argv) {
-  if (argc != 3) {
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
     printf("Usage: %s seed arraysize\n", argv[0]);
-    return 1;
+    return -1;
   }
+  char *help = malloc(sizeof(char) * (strlen(argv[1]) + 1));
+  strcpy(help, argv[1]);
 
-  int seed = atoi(argv[1]);
+  int seed = atoi(help);
   if (seed <= 0) {
     printf("seed is a positive number\n");
     return 1;
   }
 
-  int array_size = atoi(argv[2]);
+  int array_size = atoi(help);
   if (array_size <= 0) {
     printf("array_size is a positive number\n");
     return 1;
