f29ad1d4c553d850276eeed68a25133331abf3b4
astronavigator98 <astronavigator98@gmail.com>
2019-11-17 13:06:35 +0000

    Выполнена лабораторная работа 4

---
+++ lab4/src/sum.c
@@ -0,0 +1,11 @@
+#include "sum.h"
+
+int Sum(const struct SumArgs *args) {
+  int sum = 0;
+  // TODO: your code here
+  for (int i=args->begin;i<=args->end;i++){
+      sum+=args->array[i];
+  }
+  //printf("%d\n",sum);
+  return sum;
+}
